e5a2e4d600228ce40d96d4221c0c8534608e4226
yuaquintero <yudy.quintero@udea.edu.co>
2025-10-30 23:19:48 -0500

    Implementacion de la clase particula

--- Simulacion/main.cpp
+++ Simulacion/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <particula.h>
+#include <fstream>      // std::ofstream
+
+#define PI 3.14159265
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    double x,y,v,ang;
+    ofstream ofs ("Calculos.txt", std::ofstream::out);
+    cout << "******* Simulacion movimiento parabolico ********" << endl;
+    cout <<"Ingrese las condiciones iniciales"<<endl;
+    cout <<"Ingrese la posicion inicial en x"<<endl;
+    cin>>x;
+    cout <<"Ingrese la posicion inicial en y"<<endl;
+    cin>>y;
+    cout <<"Ingrese la velocidad inicial"<<endl;
+    cin>>v;
+    cout <<"Ingrese el angulo inicial"<<endl;
+    cin>>ang;
+    double rad=(ang*PI)/180;
+    Particula miParticula(x,y,v,rad);
+
+    ofs<<miParticula.getX()<<'\t'<<miParticula.getY()<<endl;
+    while(miParticula.getY()>=0)
+    {
+        miParticula.CalcularVelocidad();
+        miParticula.CalcularPosicion();
+        miParticula.ActualizarVelocidad();
+        ofs<<miParticula.getX()<<'\t'<<miParticula.getY()<<endl;
+    }
+    ofs.close();
+    cout<<"Simulación terminada"<<endl;
+
+
+
     return 0;
 }
